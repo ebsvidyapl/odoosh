05c0e274084e49981a21131a08ca1ea9072fa473
Vidhya <ebsvidyapl@gmail.com>
2026-03-31 11:25:01 +0530

    add new ACL files bug fix in manifest

--- custom/addons/employee_management/__manifest__.py
+++ custom/addons/employee_management/__manifest__.py
@@ -6,10 +6,16 @@
     'category': 'Human Resources',
     'depends': ['base','sale', 'hr', 'purchase'],
     'data': [
-        'security/ir.model.access.csv',
+        'security/security.xml',        
+        'security/ir.model.access.csv', 
+        'security/record_rules.xml',
+        
         'data/employee_sequence.xml',
+
         'reports/employee_report.xml',
         'reports/employee_report_template.xml',
+        
+        'views/res_partner_views.xml',
         'views/employee_views.xml',
         'views/sale_order_views.xml',      
         'views/purchase_order_views.xml',
