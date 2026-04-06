bc370ba9ed963043333eb80f13cd1d06a0577290
Vidhya <ebsvidyapl@gmail.com>
2026-04-06 14:56:30 +0530

    hide crm module for accountant-

--- custom/addons/employee_management/__manifest__.py
+++ custom/addons/employee_management/__manifest__.py
@@ -5,7 +5,7 @@
     'author': 'Vidhya',
     'category': 'Human Resources',
     'license': 'LGPL-3',
-    'depends': ['base','sale', 'hr', 'purchase'],
+    'depends': ['base','sale', 'hr', 'purchase' , 'crm'],
     'data': [
         'security/security.xml',        
         'security/ir.model.access.csv',
