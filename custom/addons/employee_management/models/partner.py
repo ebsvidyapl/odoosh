eb9fc2495dec41c560d82b1ede5539d441676522
Vidhya <ebsvidyapl@gmail.com>
2026-04-07 15:58:24 +0530

    check error in parner.py-1

--- custom/addons/employee_management/models/partner.py
+++ custom/addons/employee_management/models/partner.py
@@ -1,5 +1,4 @@
-from odoo import models, fields, api
-from odoo.exceptions import AccessError
+from odoo import models, fields
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -7,29 +6,4 @@ class ResPartner(models.Model):
     approval_state = fields.Selection([
         ('draft', 'Pending Approval'),
         ('approved', 'Approved')
-    ], default='draft', string="Approval Status")
-
-    @api.model
-    def create(self, vals):
-        if not vals.get('approval_state'):
-            user = self.env.user
-
-            if user.has_group('employee_management.group_sales_officer') or \
-               user.has_group('employee_management.group_logistics'):
-                vals['approval_state'] = 'draft'
-            else:
-                vals['approval_state'] = 'approved'
-
-        return super().create(vals)
-
-    def write(self, vals):
-        if 'approval_state' not in vals:
-            for rec in self:
-                if rec.approval_state != 'approved' and not self.env.user.has_group('employee_management.group_management'):
-                    raise AccessError("You cannot modify unapproved customers/vendors.")
-        return super().write(vals)
-
-    def action_approve(self):
-        if not self.env.user.has_group('employee_management.group_management'):
-            raise AccessError("Only Management can approve.")
-        self.write({'approval_state': 'approved'})
+    ], default='draft')
