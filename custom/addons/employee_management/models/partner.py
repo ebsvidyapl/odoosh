bd4fd29a9ffcc3adc814363342a6ba02f70445a6
Vidhya <ebsvidyapl@gmail.com>
2026-04-07 15:31:42 +0530

    updated partner.py

--- custom/addons/employee_management/models/partner.py
+++ custom/addons/employee_management/models/partner.py
@@ -8,25 +8,28 @@ class ResPartner(models.Model):
         ('draft', 'Pending Approval'),
         ('approved', 'Approved')
     ], default='draft', string="Approval Status")
+
     @api.model
     def create(self, vals):
-        user = self.env.user
+        if not vals.get('approval_state'):
+            user = self.env.user
 
-        if user.has_group('employee_management.group_sales_officer') or \
-           user.has_group('employee_management.group_logistics'):
-            vals['approval_state'] = 'draft'
-        else:
-            vals['approval_state'] = 'approved'
+            if user.has_group('employee_management.group_sales_officer') or \
+               user.has_group('employee_management.group_logistics'):
+                vals['approval_state'] = 'draft'
+            else:
+                vals['approval_state'] = 'approved'
 
         return super().create(vals)
+
     def write(self, vals):
-         if 'approval_state' not in vals:
+        if 'approval_state' not in vals:
             for rec in self:
                 if rec.approval_state != 'approved' and not self.env.user.has_group('employee_management.group_management'):
                     raise AccessError("You cannot modify unapproved customers/vendors.")
-                return super().write(vals)
+        return super().write(vals)
+
     def action_approve(self):
         if not self.env.user.has_group('employee_management.group_management'):
             raise AccessError("Only Management can approve.")
-        self.write({'approval_state': 'approved'})  
-    
+        self.write({'approval_state': 'approved'})
