77432601c919ddb2ecd3912ffefd75cc1864a70d
Vidhya <ebsvidyapl@gmail.com>
2026-04-09 16:36:53 +0530

    remove em1

--- custom/addons/employee_management1/models/purchase.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
-
-    country_of_origin_id = fields.Many2one(
-        'res.country',
-        string="Country of Origin"
-    )
