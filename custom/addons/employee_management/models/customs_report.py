a47c58307b1a2fa52eeaab5a37f7bd4ebcfa88dd
Vidhya <ebsvidyapl@gmail.com>
2026-04-11 12:06:40 +0530

    Added hs code edit stock

--- custom/addons/employee_management/models/customs_report.py
+++ custom/addons/employee_management/models/customs_report.py
@@ -2,6 +2,7 @@ from odoo import models
 
 class CustomsReport(models.AbstractModel):
     _name = 'report.customs_management.report_customs'
+    _description = 'Customs Report'
 
     def _get_report_values(self, docids, data=None):
         records = self.env['stock.landed.cost'].browse(docids)
--- custom/addons/employee_management/models/stock_landed_cost.py
+++ custom/addons/employee_management/models/stock_landed_cost.py
@@ -1,5 +1,6 @@
 from odoo import models
 
+
 class StockLandedCost(models.Model):
     _inherit = 'stock.landed.cost'
 
@@ -9,25 +10,27 @@ class StockLandedCost(models.Model):
             ('country_id', '=', product.country_of_origin_id.id),
             ('active', '=', True)
         ], limit=1)
-
         return rule
 
-    def button_validate(self):
-        res = super().button_validate()
+    def _get_valuation_lines(self):
+        lines = super()._get_valuation_lines()
 
-        for cost in self:
-            for line in cost.valuation_adjustment_lines:
-                product = line.product_id
-                rule = self._get_customs_exemption(product)
+        for line in lines:
+            product = line.get('product_id')
+            if not product:
+                continue
 
-                if rule:
-                    if rule.exemption_type == 'full':
-                        line.additional_landed_cost = 0
-                    elif rule.exemption_type == 'partial':
-                        reduction = (
-                            line.additional_landed_cost *
-                            rule.exemption_percentage / 100
-                        )
-                        line.additional_landed_cost -= reduction
+            product_obj = self.env['product.product'].browse(product)
+            rule = self._get_customs_exemption(product_obj)
 
-        return res
+            if rule:
+                additional_cost = line.get('additional_landed_cost', 0)
+
+                if rule.exemption_type == 'full':
+                    line['additional_landed_cost'] = 0
+
+                elif rule.exemption_type == 'partial':
+                    reduction = additional_cost * rule.exemption_percentage / 100
+                    line['additional_landed_cost'] = additional_cost - reduction
+
+        return lines
