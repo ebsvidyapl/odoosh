84a73e0957e8a23baebb5309e9f69f8c9482100e
Vidhya <ebsvidyapl@gmail.com>
2026-04-11 14:03:38 +0530

    Added computed landed cost1

--- custom/addons/employee_management/models/stock_landed_cost.py
+++ custom/addons/employee_management/models/stock_landed_cost.py
@@ -4,15 +4,15 @@ from odoo import models
 class StockLandedCost(models.Model):
     _inherit = 'stock.landed.cost'
 
-    def _compute_landed_cost(self):
-        """
-        Override final computation step to apply exemption
-        """
-        res = super()._compute_landed_cost()
+    def _get_landed_cost_lines(self):
+        lines = super()._get_landed_cost_lines()
 
         for cost in self:
-            for line in cost.valuation_adjustment_lines:
-                product = line.product_id
+            for cost_line in cost.cost_lines:
+                product = cost_line.product_id
+
+                if not product:
+                    continue
 
                 hs_code = (product.product_tmpl_id.hs_code or '').replace('.', '').strip()
                 country_id = product.country_of_origin_id.id
@@ -25,10 +25,10 @@ class StockLandedCost(models.Model):
 
                 if rule:
                     if rule.exemption_type == 'full':
-                        line.additional_landed_cost = 0.0
+                        cost_line.price_unit = 0.0
 
                     elif rule.exemption_type == 'partial':
-                        reduction = line.additional_landed_cost * (rule.exemption_percentage / 100.0)
-                        line.additional_landed_cost -= reduction
+                        reduction = cost_line.price_unit * (rule.exemption_percentage / 100.0)
+                        cost_line.price_unit -= reduction
 
-        return res
+        return lines
